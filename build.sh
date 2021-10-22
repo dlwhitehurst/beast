57ffcc1b271b2608173f7cd14cfc7e725af1d29b
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-10-22 09:13:03 -0400

    serious refactor

---
+++ build.sh
@@ -0,0 +1,1 @@
+podman run -it --rm -v $PWD:$PWD:Z -w $PWD localhost/beast-buildtools:latest
--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-podman run -it --rm -v $PWD:$PWD:Z -w $PWD localhost/beast-buildtools:latest
+#!/bin/bash
+# run.sh
+# 
+# This script starts a Linux container mapped to your Tavros git clone.
+# The container shell presented will contain various tools and dependencies
+# needed to work on Tavros. This saves much explanation and time.
+#
+ 
+docker run -it --rm -v $PWD:$PWD:Z -w $PWD ghcr.io/ms3inc/tavros-collection:latest
